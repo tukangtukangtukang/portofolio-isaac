3bb7a93852b87aee7f65174ceff15059bffe531c
Isaac <bonaventuraisaac52@gmail.com>
2026-02-03 08:06:42 +0800

    fix: remove Vercel adapter to resolve build error

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
-import edgeoneAdapter from '@edgeone/astro';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: edgeoneAdapter(),
   output: 'static',
-  integrations: [react()]
+  integrations: [react()],
 });
